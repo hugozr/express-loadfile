e5957c6397759662ba8d1266d91aec1bf3fb3e3a
hzumaeta <hzumaeta@gmail.com>
2024-10-14 06:11:52 -0500

    carga masiva de veterinarias

--- index.ts
+++ index.ts
@@ -4,20 +4,50 @@ import express from 'express';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as chokidar from 'chokidar';
-
+const axios = require('axios');
 const app = express();
 const port = 3005;
 
 const folderPath = 'C:/ehzr/16.Petso/02.app/payload-petzocial/src/excel-files';
-const subfolderPath = 'C:/ehzr/16.Petso/02.app/payload-petzocial/src/excel-files/processed';
+const subfolderPath = 'C:/ehzr/16.Petso/02.app/payload-petzocial/src/excel-files-processed';
+const API_URL = 'http://localhost:3000/api/vets';  // Cambia esto
+
 
 function moveExcelFile(filePath: string) {
+  console.log("voy a mover");
   const fileName = path.basename(filePath);
   const destinationPath = path.join(subfolderPath, fileName);
-
   fs.renameSync(filePath, destinationPath);
-
   console.log(`Archivo movido: ${fileName}`);
+}
+
+async function loadData(){
+  console.log("Estoy aca... ");
+  try{
+    const data: any = JSON.parse(fs.readFileSync('upload/vets/mass-load-vets.json', 'utf-8'));
+    console.log(data);
+    for (const item of data) {
+      await uploadItem(item);
+    }
+    console.log('Carga masiva completada.');
+  }catch{
+    console.log("error al cargar el archivo")
+  }
+  
+}
+
+async function uploadItem(item: any) {
+  try {
+    const response = await axios.post(API_URL, item, {
+      headers: {
+        'Authorization': "AUTH_TOKEN",
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log(`Item cargado con éxito: ${response.data.id}`);
+  } catch (error: any) {
+    console.error('Error al cargar el item:', error.response ? error.response.data : error.message);
+  }
 }
 
 function watchFolder() {
@@ -25,12 +55,12 @@ function watchFolder() {
     ignored: /(^|[/\\])\../, // Ignorar archivos ocultos
     persistent: true,
   });
-
   console.log(`Observando la carpeta: ${folderPath}`);
 
   watcher
-    .on('add', (filePath) => {
+    .on('add', async (filePath) => {
       if (path.extname(filePath) === '.xlsx') {
+        await loadData();
         moveExcelFile(filePath);
       }
     })
